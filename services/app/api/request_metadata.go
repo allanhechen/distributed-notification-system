373455edb3a200e69c647b7e1b31609e987ad9c0
Allan He-Chen <allanhechen@gmail.com>
2025-12-08 20:06:14 -0500

    Read request metadata

---
+++ services/app/api/request_metadata.go
@@ -0,0 +1,76 @@
+package api
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type RequestIdentifiers struct {
+	jwt.RegisteredClaims
+	RequestId uuid.UUID `json:"request_id"`
+	UserId    uuid.UUID `json:"user_id"`
+}
+
+// Unique keys for the metadata context
+type metadataContextKey string
+
+const requestId metadataContextKey = "requestId"
+const userId metadataContextKey = "userId"
+
+// RequestMetadataMiddleware retrieves the identifying information for the
+// current request from its associated JWT, and immediately rejects any
+// requests with invalid identifiers. Additional information about the
+// identification fields can be found in the [logging documentation].
+//
+// The request has already been verified at this point, so we can skip
+// checking the JWT signature. We also know that it exists, and is valid.
+//
+// [logging documentation]: https://github.com/allanhechen/distributed-notification-system/blob/main/docs/observability/logging.md
+func RequestMetadataMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		auth := req.Header.Get("Authorization")
+		if auth == "" {
+			http.Error(w, "missing authorization header", http.StatusUnauthorized)
+			slog.Error("request received without authorization header")
+			os.Exit(1)
+			return
+		}
+
+		const prefix = "Bearer "
+		if !strings.HasPrefix(auth, prefix) {
+			http.Error(w, "invalid authorization header", http.StatusUnauthorized)
+			slog.Error("request received invalid authorization header")
+			os.Exit(1)
+			return
+		}
+
+		headerToken := strings.TrimPrefix(auth, prefix)
+		token, _, err := new(jwt.Parser).ParseUnverified(headerToken, &RequestIdentifiers{})
+		if err != nil {
+			http.Error(w, "invalid JWT", http.StatusUnauthorized)
+			slog.Error("request received with invalid JWT", "error", err)
+			os.Exit(1)
+		}
+
+		claims, ok := token.Claims.(*RequestIdentifiers)
+		if !ok {
+			http.Error(w, "invalid JWT", http.StatusUnauthorized)
+			slog.Error("request received with invalid JWT")
+			os.Exit(1)
+			return
+		}
+
+		ctx := req.Context()
+		ctx = context.WithValue(ctx, requestId, claims.RequestId)
+		ctx = context.WithValue(ctx, userId, claims.UserId)
+		req = req.WithContext(ctx)
+
+		next.ServeHTTP(w, req)
+	}
+}
