ebf269e523a1100a144e978b26f25f7b83cdfd18
Allan He-Chen <allanhechen@gmail.com>
2025-12-11 01:07:34 -0500

    Update docstrings and function

--- services/app/db/idempotency.go
+++ services/app/db/idempotency.go
@@ -2,27 +2,37 @@ package db
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/allanhechen/distributed-notification-system/services/app/db/generated"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// Idempotency is the group of queries related to handling request
+// idempotency.
 type Idempotency struct {
 	pool *pgxpool.Pool
 }
 
-func (i *Idempotency) GetStoredRequest(requestId uuid.UUID) (*generated.IdempotentRequest, error) {
-	ctx := context.Background()
-	tx, err := i.pool.Acquire(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	q := generated.New(tx)
+// GetStoredRequest checks the database for a stored request with the
+// given requestId.
+//
+// Returns an error that wraps ErrNoRows if no record is found for the
+// given requestId. Callers should check for this error for determining
+// if a request is new. Failed RequestStatusIds on the returned struct
+// should also be taken into consideration for retrying requests.
+func (i *Idempotency) GetStoredRequest(ctx context.Context, requestId uuid.UUID) (*generated.IdempotentRequest, error) {
+	q := generated.New(i.pool)
 	res, err := q.GetRequestStatus(ctx, requestId)
 	if err != nil {
-		return nil, err
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrNoRows
+		}
+
+		return nil, fmt.Errorf("db: failed to retrieve stored request %s: %w", requestId, err)
 	}
 
 	return &res, nil
--- utils/types/idempotency.go
+++ utils/types/idempotency.go
@@ -1,5 +1,9 @@
 package types
 
+// Request status type for the idempotent_request_statuses table in the
+// application server.
+//
+// sqlc generates code pointing to this status.
 type RequestStatus int
 
 const (
