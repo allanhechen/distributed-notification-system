c61a67dfb1d7d640300a5fc995105ca042d8bdea
Allan He-Chen <allanhechen@gmail.com>
2025-12-25 21:45:49 -0500

    Support reconnect with this implementation

--- services/worker/internal/services/rabbitmq_consumer.go
+++ services/worker/internal/services/rabbitmq_consumer.go
@@ -2,88 +2,157 @@ package services
 
 import (
 	"context"
-	"crypto/rand"
-	"encoding/hex"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"log/slog"
+	"sync"
 	"time"
 
 	"github.com/allanhechen/distributed-notification-system/services/worker/internal/domain"
 	rabbitmqnotifications "github.com/allanhechen/distributed-notification-system/utils/rabbitmq_notifications"
+	"github.com/google/uuid"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func generateRandomSuffix() string {
-	b := make([]byte, 8)
-	rand.Read(b)
-	return hex.EncodeToString(b)
-}
+var ErrConnection = errors.New("consumer: communication dropped with rabbitmq")
 
 type RabbitMqConsumer struct {
-	conn *amqp.Connection
+	url             string
+	maxRetryTimeout uint
+	healthyTimeout  uint
+	prefetch        uint
+}
+
+func NewRabbitMqConsumer(url string, maxRetryTimeout uint, healthyTimeout uint, prefetch uint) domain.Consumer[domain.Notification] {
+	return &RabbitMqConsumer{
+		url:             url,
+		maxRetryTimeout: maxRetryTimeout,
+		healthyTimeout:  healthyTimeout,
+		prefetch:        prefetch,
+	}
 }
 
 func (r *RabbitMqConsumer) Consume(ctx context.Context) (<-chan domain.Message[domain.Notification], error) {
-	channel, err := r.conn.Channel()
-	if err != nil {
-		return nil, err
-	}
-
-	if err := channel.Qos(10, 0, false); err != nil {
-		channel.Close()
-		return nil, err
-	}
-
-	consumer := fmt.Sprintf("%s-%d-%s", rabbitmqnotifications.TestNotificationQueue, time.Now().UTC().UnixMilli(), generateRandomSuffix())
-	inputCh, err := channel.Consume(string(rabbitmqnotifications.TestNotificationQueue), consumer, false, false, false, false, nil)
-	if err != nil {
-		channel.Close()
-		return nil, err
-	}
 	outputCh := make(chan domain.Message[domain.Notification])
 
-	go func() {
-		defer close(outputCh)
-		defer channel.Close()
-		defer channel.Cancel(consumer, false)
+	go r.handleReconnect(ctx, outputCh)
+	return outputCh, nil
+}
 
-		for {
+func (r *RabbitMqConsumer) handleReconnect(ctx context.Context, outputCh chan domain.Message[domain.Notification]) {
+	defer close(outputCh)
+
+	backoff := uint(1)
+	for {
+		if ctx.Err() != nil {
+			slog.Info("consumer: received context close signal, exiting cleanly")
+			return
+		}
+
+		begin := time.Now()
+		slog.Info("consumer: (re)connecting to rabbitmq")
+		r.handleIteration(ctx, outputCh)
+
+		runningTime := time.Since(begin)
+		if runningTime <= time.Duration(r.healthyTimeout*uint(time.Second)) {
+			backoff = min(backoff*2, r.maxRetryTimeout)
+			slog.Info("consumer: previous iteration failed, waiting for backoff", "backoff", backoff)
+
 			select {
 			case <-ctx.Done():
+				slog.Info("consumer: context closed while waiting for timeout")
 				return
-			case d, ok := <-inputCh:
-				if !ok {
-					slog.Warn("consumer: channel closed before context closed")
-					return
-				}
+			case <-time.After(time.Duration(backoff) * time.Second):
+			}
+		} else {
+			backoff = uint(1)
+			slog.Info("consumer: previous healthy connection dropped, resetting backoff")
+		}
+	}
+}
 
-				var payload domain.Notification
-				err := json.Unmarshal(d.Body, &payload)
-				if err != nil {
-					d.Nack(false, false)
-					slog.Error("consumer: failed to unmarshal message body", "body", d.Body)
-					continue
-				}
+func (r *RabbitMqConsumer) handleIteration(ctx context.Context, outputCh chan domain.Message[domain.Notification]) error {
+	conn, err := amqp.Dial(r.url)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
 
-				notification := RabbitmqNotification{
-					payload:    payload,
-					identifier: d.MessageId,
-					ackFn: func(ctx context.Context) error {
-						return d.Ack(false)
-					},
-					nackFn: func(ctx context.Context, requeue bool) error {
-						return d.Nack(false, requeue)
-					},
-				}
+	channel, err := conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer channel.Close()
 
-				select {
-				case <-ctx.Done():
-					d.Nack(false, true)
-				case outputCh <- &notification:
-				}
+	if err := channel.Qos(int(r.prefetch), 0, false); err != nil {
+		return err
+	}
+
+	consumer := uuid.New().String()
+	inputCh, err := channel.Consume(string(rabbitmqnotifications.TestNotificationQueue), consumer, false, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+	defer channel.Cancel(consumer, false)
+
+	chanCh := make(chan *amqp.Error)
+	connCh := make(chan *amqp.Error)
+	channel.NotifyClose(chanCh)
+	conn.NotifyClose(connCh)
+
+	var wg sync.WaitGroup
+	shutdownCh := make(chan struct{})
+
+	for {
+		select {
+		case <-ctx.Done():
+			slog.Info("consumer: context cancelled, waiting for in-flight messages")
+			close(shutdownCh)
+			wg.Wait()
+			slog.Info("consumer: all in-flight messages processed")
+			return nil
+		case <-chanCh:
+			close(shutdownCh)
+			wg.Wait()
+			return ErrConnection
+		case <-connCh:
+			close(shutdownCh)
+			wg.Wait()
+			return ErrConnection
+		case d, ok := <-inputCh:
+			if !ok {
+				slog.Warn("consumer: channel closed before context closed")
+				return nil
+			}
+
+			var payload domain.Notification
+			err := json.Unmarshal(d.Body, &payload)
+			if err != nil {
+				d.Nack(false, false)
+				slog.Error("consumer: failed to unmarshal message body", "body", d.Body)
+				continue
+			}
+
+			wg.Add(1)
+			notification := RabbitmqNotification{
+				payload:    payload,
+				identifier: d.MessageId,
+				ackFn: func(ctx context.Context) error {
+					defer wg.Done()
+					return d.Ack(false)
+				},
+				nackFn: func(ctx context.Context, requeue bool) error {
+					defer wg.Done()
+					return d.Nack(false, requeue)
+				},
+			}
+
+			select {
+			case <-shutdownCh:
+				d.Nack(false, true)
+				wg.Done()
+			case outputCh <- &notification:
 			}
 		}
-	}()
-	return outputCh, nil
+	}
 }
