cea7ad1a64eb3201c25259761c913c8bc2b17af7
Allan He-Chen <allanhechen@gmail.com>
2025-12-28 13:54:08 -0500

    Update publish messages to include context checks and better error messages

--- services/worker/internal/testutil/publish_messages.go
+++ services/worker/internal/testutil/publish_messages.go
@@ -10,32 +10,50 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func PublishMessages(ctx context.Context, url string, notifications []domain.Notification, exchange rabbitmqnotifications.ExchangeName, routingKey rabbitmqnotifications.RoutingKey) error {
+// PublishMessages is a context-aware message publisher intended to be
+// used in integration tests to publish notifications to a RabbitMQ
+// instance.
+func PublishMessages(
+	ctx context.Context,
+	url string,
+	notifications []domain.Notification,
+	exchange rabbitmqnotifications.ExchangeName,
+	routingKey rabbitmqnotifications.RoutingKey,
+) error {
 	conn, err := amqp.Dial(url)
 	if err != nil {
+		slog.Error("publish messages: failed to open connection")
 		return err
 	}
 	defer conn.Close()
 
 	channel, err := conn.Channel()
 	if err != nil {
+		slog.Error("publish messages: failed to open channel")
 		return err
 	}
 	defer channel.Close()
 
 	for _, n := range notifications {
+		if ctx.Err() != nil {
+			slog.Error("publish messages: context cancelled while publishing messages")
+			return ctx.Err()
+		}
+
 		body, err := json.Marshal(n)
 		if err != nil {
+			slog.Error("publish messages: failed marshal payload")
 			return err
 		}
-		slog.Info("publish messages: publishing message", "exchange", exchange, "routingKey", routingKey)
 		err = channel.PublishWithContext(ctx, string(exchange), string(routingKey), false, false, amqp.Publishing{
 			ContentType: "application/json",
 			Body:        body,
 		})
 		if err != nil {
+			slog.Error("publish messages: failed to publish message", "exchange", exchange, "routingKey", routingKey, "body", body)
 			return err
 		}
+		slog.Info("publish messages: published message", "exchange", exchange, "routingKey", routingKey, "body", body)
 	}
 
 	return nil
